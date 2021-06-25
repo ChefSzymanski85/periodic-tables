cdc6f897db0eaa76107b8e30b72b1024f75afce7
Mark Szymanski <chefszymanski85@gmail.com>
2021-06-25 14:39:40 -0400

    Fixed duplicated api.js file

--- front-end/src/ReservationComponents/ReservationCreate.js
+++ front-end/src/ReservationComponents/ReservationCreate.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { createReservation } from "./api";
+import { createReservation } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 
 function ReservationCreate() {
